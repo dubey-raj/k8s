96da6a35fef23e041b1ceeef5de905f8aba6cf36
dubey-raj <dubey.rajfgp@gmail.com>
2024-11-17 14:35:53 +0530

    Access given for modify security group

--- infrastructure/terraform/bootstrap/pipelinebuild/modules/tfbootstrap/sampleappIAM.tf
+++ infrastructure/terraform/bootstrap/pipelinebuild/modules/tfbootstrap/sampleappIAM.tf
@@ -105,7 +105,8 @@ data "aws_iam_policy_document" "SampleApp" {
       "ec2:AuthorizeSecurityGroupIngress",
       "ec2:CreateSecurityGroup",
       "ec2:DeleteSecurityGroup",
-      "ec2:RevokeSecurityGroupEgress"
+      "ec2:RevokeSecurityGroupEgress",
+      "ec2:RevokeSecurityGroupIngress"
     ]
     resources = [
       "arn:aws:ec2:${var.Region}:${data.aws_caller_identity.current.account_id}:security-group/*"
